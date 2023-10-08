7726cbc7d3ad1db0ea91f5799f7e132481005c55
nano <lndangkhoa19@gmail.com>
2023-10-08 14:25:22 +0700

    Add register b

---
+++ register_b.v
@@ -0,0 +1,16 @@
+module register_b (
+    input clk, reset, load;
+    input [7:0] in;
+    output [7:0] out;
+)
+
+reg [7:0] register_b;
+
+always @(posedge clk) begin
+    if (reset) register_b <= 8'b0;
+    else if (load) register_b <= in;
+end
+
+assign out = register_b;
+
+endmodule
