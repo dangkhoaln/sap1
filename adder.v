9295def4e7b28574b3a31c542e8981a23f571841
nano <lndangkhoa19@gmail.com>
2023-10-08 14:51:24 +0700

    Modify adder

--- adder.v
+++ adder.v
@@ -1,18 +1,9 @@
 module adder (
-    input clk, reset,
     input [7:0] a_word, b_word,
     input sum, // 1 for Add, 0 for Sub
     output [7:0] out
 );
 
-reg [7:0] adder;
-
-always @(posedge clk) begin
-    if (reset) adder <= 8'b0;
-    else if (sum) adder <= a_word + b_word;
-    else adder <= a_word - b_word;
-end
-
-assign out = adder;
+assign out = (sum) ? a_word + b_word : a_word - b_word;
 
 endmodule
