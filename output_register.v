9a647d035e60a2b760930892fa524049c3329a9b
nano <lndangkhoa19@gmail.com>
2023-10-08 14:27:45 +0700

    Add output_register

---
+++ output_register.v
@@ -0,0 +1,16 @@
+module (
+    input clk, reset, load;
+    input [7:0] in;
+    output [7:0] out;
+)
+
+reg [7:0] output_register;
+
+always @(posedge clk) begin
+    if (reset) output_register <= 8'b0;
+    else if (load) output_register <= in;
+end
+
+assign out = output_register;
+
+endmodule
