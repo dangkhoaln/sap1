a99babf354ebfcb4b3b4e594c9f7fb47ceb366d9
nano <lndangkhoa19@gmail.com>
2023-10-08 11:38:04 +0700

    Finish program_counter

--- program_counter.v
+++ program_counter.v
@@ -1,13 +1,15 @@
 module program_counter (
-    input clk, reset, increment;
-    output out;
-)
+    input clk, reset, increment,
+    output[3:0] out
+);
 
-reg [3:0] count;
+reg[3:0] count;
 
 always @(posedge clk) begin
     if (reset) count <= 4'b0;
     else if (increment) count <= count + 1;
 end
+
+assign out = count;
 
 endmodule
