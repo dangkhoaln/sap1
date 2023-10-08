fb2da5879d540beec58b9c8ac087722dfd6f56c4
nano <lndangkhoa19@gmail.com>
2023-10-08 14:20:37 +0700

    Add accumulator_a.v

---
+++ accumulator_a.v
@@ -0,0 +1,14 @@
+module accumulator_a (
+    input clk, reset, load;
+    input [7:0] in;
+    output [7:0] out;
+)
+
+reg accumulator_a;
+
+always @(posedge clk) begin
+    if (reset) accumulator_a <= 8'b0;
+    else if (load) accumulator_a <= in; 
+end
+
+assign out = accumulator_a;
