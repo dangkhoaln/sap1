872b7b183073dd9333c7be3ea2a187d390cd792d
nano <lndangkhoa19@gmail.com>
2023-10-08 14:11:33 +0700

    Add memory and instruction register

---
+++ instruction_register.v
@@ -0,0 +1,16 @@
+module instruction_register (
+    input clk, reset, load, enable;
+    input [7:0] in;
+    output [7:0] out;
+)
+
+reg [7:0] instruction;
+
+always @(posedge clk) begin
+    if (reset) instruction <= 8'b0;
+    else if (load) instruction <= in;
+end
+
+assign out = instruction;
+
+endmodule
---
+++ memory.v
@@ -0,0 +1,19 @@
+module memory(
+    input clk,reset,load;
+    input[7:0] bus;
+    output[7:0] out
+)
+
+initial begin
+    $readmemh("program.bin",ram);
+end
+
+reg[3:0] mar; // Memory address register
+reg[7:0] ram[0:15];
+
+always @(posedge clk) begin
+    if (reset) mar <= 4'b0;
+    else if (load) mar <= bus[3:0];
+end
+
+assign out = ram[mar];
